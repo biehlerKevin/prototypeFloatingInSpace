f0d0dc280a4fff40f9c66844b7f591f3a46b7a90
Kevin Biehler <kevin_biehler@hotmail.com>
2017-03-12 16:31:37 +0100

    updated prototype

---
+++ Assets/_game/Scripts/Miscellaneous/EllipticalCurve.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EllipticalCurve : MonoBehaviour {
+
+    public float a;
+    public float b;
+    public float x;
+    public float y;
+    public float alpha;
+    public float X;
+    public float Y;
+
+     
+     
+    void Update()
+    {
+        alpha += 10;
+        ;
+        X = x + (a * Mathf.Cos(alpha * .005f));
+        Y = y + (b * Mathf.Sin(alpha * .005f));
+        this.gameObject.transform.position = new Vector3(X, 4.5f, Y);
+    }
+}
